e5389d67226a3ccfe244bf9b81eb352e70ccf41a
Virender Kumar <34500966+virenderk91@users.noreply.github.com>
2020-11-26 12:42:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends React.Component {
 
 console.log('time', time);
 
-    if (time >= 4 && time <= 12) {
+    if (time >= 4 && time < 12) {
       greeting = "Good Morning";
       img= 'https://www.happybirthdaymsg.com/wp-content/uploads/2015/11/good-morning-image-36.jpg';
-    } else if (time > 12 && time <= 16 ) { 
+    } else if (time >= 12 && time <= 16 ) { 
       greeting = "Good Afternoon";
       img= 'https://bit.ly/3eLewI2';
     } else if(time > 16 && time <= 20) {
